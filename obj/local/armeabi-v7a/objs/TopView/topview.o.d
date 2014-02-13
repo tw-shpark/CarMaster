f6dad53e0caacb7204fae78103cccf24d8bc2cc9
shpark <shpark@teleworks.co.kr>
2014-02-13 17:21:57 +0900

    add video native processing func

--- obj/local/armeabi-v7a/objs/TopView/topview.o.d
+++ obj/local/armeabi-v7a/objs/TopView/topview.o.d
@@ -1,1 +1,151 @@
-obj/local/armeabi-v7a/objs/TopView/topview.o: jni/topview.cpp
+obj/local/armeabi-v7a/objs/TopView/topview.o: jni/topview.cpp \
+ /Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/jni.h \
+ /Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ /Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ ../../../OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp \
+ ../../../OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h \
+ /Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/assert.h \
+ /Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/stdlib.h \
+ /Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/string.h \
+ /Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/malloc.h \
+ /Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/alloca.h \
+ /Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/strings.h \
+ /Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ /Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/stdint.h \
+ /Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ /Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ /Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ /Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ /Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ /Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ /Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ /Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ /Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ /Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/memory.h \
+ /Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/math.h \
+ /Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/limits.h \
+ /Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/sys/limits.h \
+ /Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/linux/limits.h \
+ /Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/machine/internal_types.h \
+ /Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/machine/limits.h \
+ /Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/sys/syslimits.h \
+ /Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/asm/page.h \
+ ../../../OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp \
+ /Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/sources/cxx-stl/gnu-libstdc++/4.6/include/algorithm \
+ /Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath \
+ /Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef \
+ /Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/sources/cxx-stl/gnu-libstdc++/4.6/include/complex \
+ /Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/sources/cxx-stl/gnu-libstdc++/4.6/include/map \
+ /Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/sources/cxx-stl/gnu-libstdc++/4.6/include/vector \
+ ../../../OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp \
+ /Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/sources/cxx-stl/gnu-libstdc++/4.6/include/limits \
+ ../../../OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp \
+ ../../../OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc.hpp \
+ ../../../OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/imgproc/types_c.h \
+ ../../../OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h \
+ ../../../OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/features2d/features2d.hpp \
+ ../../../OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/flann/miniflann.hpp \
+ ../../../OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/flann/defines.h \
+ ../../../OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/flann/config.h
+
+/Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/jni.h:
+
+/Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+../../../OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp:
+
+../../../OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h:
+
+/Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/assert.h:
+
+/Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/stdlib.h:
+
+/Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/string.h:
+
+/Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/malloc.h:
+
+/Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/alloca.h:
+
+/Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/strings.h:
+
+/Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+/Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+/Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+/Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+/Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+/Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+/Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+/Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+/Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+/Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/memory.h:
+
+/Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/math.h:
+
+/Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/limits.h:
+
+/Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/sys/limits.h:
+
+/Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/linux/limits.h:
+
+/Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/machine/internal_types.h:
+
+/Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/machine/limits.h:
+
+/Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/sys/syslimits.h:
+
+/Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/platforms/android-8/arch-arm/usr/include/asm/page.h:
+
+../../../OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp:
+
+/Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/sources/cxx-stl/gnu-libstdc++/4.6/include/algorithm:
+
+/Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath:
+
+/Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef:
+
+/Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/sources/cxx-stl/gnu-libstdc++/4.6/include/complex:
+
+/Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/sources/cxx-stl/gnu-libstdc++/4.6/include/map:
+
+/Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/sources/cxx-stl/gnu-libstdc++/4.6/include/vector:
+
+../../../OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp:
+
+/Users/Nino/Desktop/adt-bundle-mac-x86_64-20131030/android-ndk-r9c/sources/cxx-stl/gnu-libstdc++/4.6/include/limits:
+
+../../../OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp:
+
+../../../OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc.hpp:
+
+../../../OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/imgproc/types_c.h:
+
+../../../OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h:
+
+../../../OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/features2d/features2d.hpp:
+
+../../../OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/flann/miniflann.hpp:
+
+../../../OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/flann/defines.h:
+
+../../../OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/flann/config.h:
--- src/com/teleworks/carmaster/CarmasterVideoFragment.java
+++ src/com/teleworks/carmaster/CarmasterVideoFragment.java
@@ -1,19 +1,104 @@
 package com.teleworks.carmaster;
 
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 import android.support.v4.app.Fragment;
 
-public class CarmasterVideoFragment extends Fragment {
+public class CarmasterVideoFragment extends Fragment implements CvCameraViewListener2 {
 	public static final String ARG_OBJECT = "object";
+	protected static final String TAG = "VidioFragment";
+	
+	private CameraBridgeViewBase mOpenCvCameraView;
+	private Mat mRgba;
+	private Mat mGray;
 
+	private BaseLoaderCallback  mLoaderCallback = new BaseLoaderCallback(getActivity()) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    Log.i(TAG, "OpenCV loaded successfully");
+
+                    // Load native library after(!) OpenCV initialization
+                    System.loadLibrary("TopView");
+
+                    mOpenCvCameraView.enableView();
+                    Toast.makeText(getActivity(), "OpenCV Manager Connected.", Toast.LENGTH_SHORT).show();
+                } break;
+                default:
+                {
+                    super.onManagerConnected(status);
+                } break;
+            }
+        }
+    };
+	
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+    
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_8, getActivity(), mLoaderCallback);
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View rootView;
 		rootView = inflater.inflate(R.layout.fragment_videoinfo, container, false);
 		
+		mOpenCvCameraView = (CameraBridgeViewBase) rootView.findViewById(R.id.OpenCvCameraView);
+		mOpenCvCameraView.setCvCameraViewListener(this);
+		
 		return rootView;
 	}
+
+	@Override
+	public void onCameraViewStarted(int width, int height) {
+		mRgba = new Mat(height, width, CvType.CV_8UC4);
+		mGray = new Mat(height, width, CvType.CV_8UC1);
+	}
+
+	@Override
+	public void onCameraViewStopped() {
+		mRgba.release();
+		mGray.release();
+	}
+
+	@Override
+	public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+		mRgba = inputFrame.rgba();
+		mGray = inputFrame.gray();
+		VideoProcessing(mGray.getNativeObjAddr(), mRgba.getNativeObjAddr());
+		return mRgba;
+	}
+	
+	public native void VideoProcessing(long matAddrGr, long matAddrRgba);
 }
