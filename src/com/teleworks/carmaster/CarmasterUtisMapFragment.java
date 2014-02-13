796db3142ff8b769f6311559f2e098aa4dfc0ea1
gunsoo <gunsoo@gunsoo-PC>
2014-02-13 10:50:48 +0900

    Utis UDP

--- src/com/teleworks/carmaster/CarmasterUtisMapFragment.java
+++ src/com/teleworks/carmaster/CarmasterUtisMapFragment.java
@@ -14,7 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 
-public class CarmasterUtisFragment extends Fragment implements
+public class CarmasterUtisMapFragment extends Fragment implements
 		MapView.OpenAPIKeyAuthenticationResultListener,
 		MapView.MapViewEventListener, MapView.CurrentLocationEventListener,
 		MapView.POIItemEventListener/*
