268d8966ed7c38c275aad10864801773f81bbec1
박상현 <shpark@teleworks.co.kr>
2014-03-26 11:08:26 +0900

    set application orientation to LANDSCAPE

--- src/com/teleworks/carmaster/Carmaster.java
+++ src/com/teleworks/carmaster/Carmaster.java
@@ -3,6 +3,7 @@ package com.teleworks.carmaster;
 import android.app.ActionBar;
 import android.app.FragmentTransaction;
 import android.app.ActionBar.Tab;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
@@ -27,6 +28,8 @@ public class Carmaster extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carmaster);
         
+        setRequestedOrientation( ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE );
+        
         actionBar.setDisplayShowHomeEnabled(false);
         actionBar.setDisplayShowTitleEnabled(false);
         
@@ -37,6 +40,7 @@ public class Carmaster extends FragmentActivity {
         mViewPager = (ViewPager) findViewById(R.id.pager);
         mViewPager.setAdapter(mCarmasterPagerAdapter);
         mViewPager.setEnabled(false);
+        mViewPager.setOffscreenPageLimit(5); // Keep all page alive
         mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
         	@Override
         	public void onPageSelected(int position) {
